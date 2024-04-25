b684f1937ed45655e3c838756319aa3cf763c905
Danjka <55081176+Danjka@users.noreply.github.com>
2024-04-25 14:26:09 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <body>
+    <div class="container">
+      <section>
+        <input type="text" class="input-group" v-model="inputName" placeholder="Введите имя" >
+        <button @click="addName()" class="btn btn-success">Добавить</button>
+      </section>
+
+      <ul class="list-group" v-for="name in nameList" :key="name.id">
+        <li class="list-group-item" v-if="name!=''">
+          {{ name }}
+          <button class="btn btn-danger" @click="() => deleteName(name)">
+            Удалить
+          </button>
+        </li>
+      </ul>
+
+    </div>
+  </body>
+</template>
+
+<script>
+export default{
+  name : 'App',
+  
+  data(){
+    return{
+      inputName: '',
+      nameList: [''],
+    }
+  },
+
+  methods:{
+    addName(){
+      this.nameList.push(this.inputName)
+      this.inputName = ""
+      localStorage.setItem('NameList',JSON.stringify(this.nameList))
+    },
+
+
+    deleteName(name){
+      const index = this.nameList.indexOf(name)
+      if(index > -1){
+        this.nameList.splice(index, 1)
+      }
+      localStorage.setItem('NameList',JSON.stringify(this.nameList))
+    },
+  },
+  
+  mounted(){
+    const data = localStorage.getItem('NameList');
+    if (data === null) return;
+    this.nameList = JSON.parse(data)
+    },
+  
+  components:{
+
+  }
+}
+
+</script>
+
+<style>
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #333333;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    background-color: #444444;
+    max-width: 700px;
+    margin: 20px auto;
+    padding: 15px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 236, 91, 0.979);
+}
+
+h1 {
+    font-size: 24px;
+}
+
+input[type="text"] {
+    width: 70%;
+    padding: 8px;
+    border-radius: 5px;
+}
+
+button {
+    padding: 10px 20px;
+    border-radius: 5px;
+    background-color: #007BFF;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 8px 0;
+    color: whitesmoke
+}
+
+.btn-danger {
+    padding: 8px;
+    border-radius: 10%;
+    background-color: #ff0000;
+    color: rgb(255, 255, 255);
+    cursor: pointer;
+}
+</style>
